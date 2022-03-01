83c979960b368d5dfd32ffa130d78a956c625ade
Gerikk <al.blt@protonmail.com>
2022-03-01 11:27:18 +0100

    test Constructor, get, illegalArgument (bad string value)

--- src/tp/color/model/Color.java
+++ src/tp/color/model/Color.java
@@ -1,4 +1,30 @@
 package tp.color.model;
 
 public class Color {
+
+    private int red;
+    private int green;
+    private int blue;
+
+
+    public Color(String hexadecimal) throws IllegalArgumentException{
+
+        java.awt.Color color =  java.awt.Color.decode(hexadecimal);
+        this.red = color.getRed();
+        this.green = color.getGreen();
+        this.blue = color.getBlue();
+
+    }
+
+    public int getRed() {
+        return this.red;
+    }
+
+    public int getGreen(){
+        return this.green;
+    }
+
+    public int getBlue(){
+        return this.blue;
+    }
 }
--- test/tp/color/model/ColorTest.java
+++ test/tp/color/model/ColorTest.java
@@ -1,7 +1,51 @@
 package tp.color.model;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColorTest {
+
+    private Color color;
+
+    @BeforeEach
+    void setUp(){
+        color = new Color("#D58D35");
+    }
+
+    @Test
+    void testConstructor(){
+        assertEquals(color.getRed(), 213, "Erreur! la valeur de Red est incorrecte");
+        assertEquals(color.getGreen(), 141, "Erreur! la valeur de Green est incorrecte");
+        assertEquals(color.getBlue(), 53, "Erreur! la valeur de Blue est incorrecte");
+
+    }
+
+    @Test
+    void testGetRed(){
+        assertEquals(color.getRed(), 213, "Erreur! la valeur de Red est incorrecte");
+    }
+
+    @Test
+    void testGetGreen(){
+        assertEquals(color.getGreen(), 141, "Erreur! la valeur de Green est incorrecte");
+    }
+
+    @Test
+    void testGetBlue(){
+        assertEquals(color.getBlue(), 53, "Erreur! la valeur de Blue est incorrecte");
+    }
+
+    @Test
+    void testConstructorThrowsExceptionIllegalArgument(){
+        assertThrows(IllegalArgumentException.class, ()-> new Color("Test"));
+    }
+
+    @AfterEach
+    void tearDown(){
+        color = null;
+    }
 
 }
